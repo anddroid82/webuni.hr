488e51e02a3fd6ccd011129fc7e7108be03a75d6
anddroid82 <anddroid82@gmail.com>
2022-02-14 15:36:11 +0100

    10. hf v4

--- hr/src/main/java/hu/webuni/hr/andro/mapper/VacationMapper.java
+++ hr/src/main/java/hu/webuni/hr/andro/mapper/VacationMapper.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import hu.webuni.hr.andro.dto.EmployeeDto;
 import hu.webuni.hr.andro.dto.VacationDto;
+import hu.webuni.hr.andro.model.Employee;
 import hu.webuni.hr.andro.model.Vacation;
 
 @Mapper(componentModel = "spring", uses = {CompanyMapper.class})
@@ -15,11 +18,18 @@ public interface VacationMapper {
 	@Mapping(target = "owner.company", ignore = true)
 	@Mapping(target = "owner.password", ignore = true)
 	@Mapping(target = "owner.roles", ignore = true)
-	@Mapping(target = "owner.superior.superior", ignore = true)
+	@Mapping(target = "owner.superior", qualifiedByName = "ownerSuperior")
+	//@Mapping(target = "owner.superior.superior", ignore = true)
 	VacationDto vacationToDto(Vacation vacation);
 	Vacation dtoToVacation(VacationDto vacation);
 	
 	List<VacationDto> vacationsToDtos(List<Vacation> vacations);
 	List<Vacation> dtosToVacations(List<VacationDto> vacations);
 	
+	@Named("ownerSuperior")
+	@Mapping(target = "superior", ignore = true)
+	@Mapping(target = "company", ignore = true)
+	@Mapping(target = "junior", ignore = true)
+	@Mapping(target = "roles", ignore = true)
+	EmployeeDto ownerSuperior(Employee e);
 }
